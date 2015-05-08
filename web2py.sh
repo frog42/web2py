c56da54ab4b4184c31a495679e1d248e1e61a543
frog42 <frog42@gmail.com>
2015-05-08 10:43:36 -0700

    Create web2py.sh
    This is the first step that sets up Apache and Web2py on your server.

---
+++ web2py.sh
@@ -0,0 +1,140 @@
+echo "This script will:
+1) install all modules need to run web2py on Ubuntu 14.04
+2) install web2py in /var/www/
+3) create a self signed sll certificate
+4) setup web2py with mod_wsgi
+5) overwrite /etc/apache2/sites-available/default
+6) restart apache.
+You may want to read this script before running it.
+Press a key to continue...[ctrl+C to abort]"
+
+read CONFIRM
+
+#!/bin/bash
+# optional
+# dpkg-reconfigure console-setup
+# dpkg-reconfigure timezoneconf
+# nano /etc/hostname
+# nano /etc/network/interfaces
+# nano /etc/resolv.conf
+# reboot now
+# ifconfig eth0
+
+echo "installing useful packages"
+echo "=========================="
+apt-get update
+apt-get upgrade
+apt-get -y install ssh
+apt-get -y install zip unzip
+apt-get -y install tar
+apt-get -y install openssh-server
+apt-get -y install build-essential
+apt-get -y install python
+#apt-get -y install python2.5
+apt-get -y install ipython
+apt-get -y install python-dev
+apt-get -y install postgresql
+apt-get -y install apache2
+apt-get -y install libapache2-mod-wsgi
+apt-get -y install python2.5-psycopg2
+apt-get -y install postfix
+apt-get -y install wget
+apt-get -y install python-matplotlib
+apt-get -y install python-reportlab
+apt-get -y install mercurial
+sudo apt-get -y install git
+/etc/init.d/postgresql restart
+
+# optional, uncomment for emacs
+# apt-get -y install emacs
+
+# optional, uncomment for backups using samba
+# apt-get -y install samba
+# apt-get -y install smbfs
+
+echo "downloading, installing and starting web2py"
+echo "==========================================="
+cd /var
+mkdir www
+cd www
+rm web2py_src.zip*
+wget http://web2py.com/examples/static/web2py_src.zip
+unzip web2py_src.zip
+sudo mv /var/www/web2py/* /var/www/
+mv handlers/wsgihandler.py ./wsgihandler.py
+chown -R testbed:testbed ./
+
+echo "setting up apache modules"
+echo "========================="
+a2enmod ssl
+a2enmod proxy
+a2enmod proxy_http
+a2enmod headers
+a2enmod expires
+a2enmod wsgi
+a2enmod rewrite # for 14.04
+mkdir /etc/apache2/ssl
+
+echo "creating a self signed certificate"
+echo "=================================="
+openssl genrsa 1024 > /etc/apache2/ssl/self_signed.key
+chmod 400 /etc/apache2/ssl/self_signed.key
+openssl req -new -x509 -nodes -sha1 -days 365 -key /etc/apache2/ssl/self_signed.key > /etc/apache2/ssl/self_signed.cert
+openssl x509 -noout -fingerprint -text < /etc/apache2/ssl/self_signed.cert > /etc/apache2/ssl/self_signed.info
+
+echo "rewriting your apache config file to use mod_wsgi"
+echo "================================================="
+echo '
+WSGIDaemonProcess testbed user=testbed group=testbed processes=1 threads=1
+<VirtualHost *:80>
+RewriteEngine On
+RewriteCond %{HTTPS} !=on
+RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]
+CustomLog /var/log/apache2/access.log common
+ErrorLog /var/log/apache2/error.log
+</VirtualHost>
+<VirtualHost *:443>
+SSLEngine on
+SSLCertificateFile /etc/apache2/ssl/self_signed.cert
+SSLCertificateKeyFile /etc/apache2/ssl/self_signed.key
+WSGIProcessGroup testbed
+WSGIScriptAlias / /var/www/wsgihandler.py
+WSGIPassAuthorization On
+<Directory /var/www>
+AllowOverride None
+Require all denied
+<Files wsgihandler.py>
+Require all granted
+</Files>
+</Directory>
+AliasMatch ^/([^/]+)/static/(?:_[\d]+.[\d]+.[\d]+/)?(.*) \
+/var/www/applications/$1/static/$2
+<Directory /var/www/applications/*/static/>
+Options -Indexes
+ExpiresActive On
+ExpiresDefault "access plus 1 hour"
+Require all granted
+</Directory>
+CustomLog /var/log/apache2/ssl-access.log common
+ErrorLog /var/log/apache2/error.log
+</VirtualHost>
+' > /etc/apache2/sites-available/default.conf # FOR 14.04
+
+sudo rm /etc/apache2/sites-enabled/* # FOR 14.04
+sudo a2ensite default # FOR 14.04
+
+# echo "setting up PAM"
+# echo "================"
+# sudo apt-get install pwauth
+# sudo ln -s /etc/apache2/mods-available/authnz_external.load /etc/apache2/mods-enabled
+# ln -s /etc/pam.d/apache2 /etc/pam.d/httpd
+# usermod -a -G shadow www-data
+
+echo "restarting apache"
+echo "================"
+
+/etc/init.d/apache2 restart
+cd /var/www
+sudo -u testbed python -c "from gluon.widget import console; console();"
+sudo -u testbed python -c "from gluon.main import save_password; save_password(raw_input('admin password: '),443)"
+echo "done!"
